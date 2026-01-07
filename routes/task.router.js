c0357c88f4b27d5e369b16eefac1530f4c8380af
Nabila Tamajnit <n.tamajnit.dev@gmail.com>
2026-01-07 10:55:43 +0100

    Modification tasks router

--- routes/task.router.js
+++ routes/task.router.js
@@ -1,42 +1,19 @@
+const taskController = require('../controllers/task.controller');
+
 const taskRouter = require('express').Router();
 
-taskRouter.get('/', (req, res) => {
-    res.send('Voici toutes les tâches', 200);
-})
+taskRouter.route('/')
+    .get(taskController.getAll)
+    .post(taskController.insert)
 
-taskRouter.get('/:id', (req, res) => {
-    const id = req.params.id;
-    res.send(`Voici la tâche numero ${id}`);
-})
-
-taskRouter.post('/', (req, res) => {
-    const taskToInsert = req.body;
-    res.send(taskToInsert, 201);
-})
-
-taskRouter.put('/:id', (req, res) => {
-    const taskId = req.params.id;
-    const taskUpdate = req.body;
-    taskUpdate.id = taskId;
-
-    res.send( taskUpdate , 200);
-})
-
-taskRouter.delete('/:id', (req, res) => {
-    res.sendStatus(204);
-})
+taskRouter.route('/:id')
+    .get(taskController.getAll)
+    .put(taskController.update)
+    .delete(taskController.delete)
+    .patch(taskController.updateStatus)
 
 
-taskRouter.get('/user/:id', (req, res) => {
-    const user = req.params.id;
-    res.send(`Voici les taches de l'utilisateur ${user}`);
-})
+taskRouter.get('/user/:id', taskController.getByUser)
 
-taskRouter.patch('/:isDone', (req, res) => {
-    const taskIsDone = req.params.isDone;
-    const taskIsDoneUpdate = req.body;
-
-    res.send( taskIsDoneUpdate , 200);
-})
 
 module.exports = taskRouter;
