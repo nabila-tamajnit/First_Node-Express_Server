ebc7795bdc2cf776935ec44b769aecf028cd5018
Nabila Tamajnit <n.tamajnit.dev@gmail.com>
2026-01-07 10:54:08 +0100

    Modification category router

--- routes/category.router.js
+++ routes/category.router.js
@@ -1,28 +1,27 @@
 const categoryRouter = require('express').Router();
 
-categoryRouter.get('/', (req, res) => {
-    res.send('Voici toutes les catégories', 200);
-})
+categoryRouter.route('/')
+    .get((req, res) => {
+        res.send('Voici toutes les catégories', 200);
+    })
+    .post((req, res) => {
+        const categoryToInsert = req.body;
+        res.send(categoryToInsert, 201);
+    })
 
-categoryRouter.get('/:id', (req, res) => {
-    res.send(`Voici la catégorie ${req.params.id}`, 200);
-})
+categoryRouter.route('/:id')
+    .get((req, res) => {
+        res.send(`Voici la catégorie ${req.params.id}`, 200);
+    })
+    .put((req, res) => {
+        const categoryId = req.params.id;
+        const categoryUpdate = req.body;
+        categoryUpdate.id = categoryId;
 
-categoryRouter.post('/', (req, res) => {
-    const categoryToInsert = req.body;
-    res.send(categoryToInsert, 201);
-})
-
-categoryRouter.put('/:id', (req, res) => {
-    const categoryId = req.params.id;
-    const categoryUpdate = req.body;
-    categoryUpdate.id = categoryId;
-
-    res.send( categoryUpdate , 200)
-})
-
-categoryRouter.delete('/:id', (req, res) => {
-    res.sendStatus(204);
-})
+        res.send( categoryUpdate , 200)
+    })
+    .delete((req, res) => {
+        res.sendStatus(204);
+    })
 
 module.exports = categoryRouter;
