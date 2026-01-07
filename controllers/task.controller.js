5b6b5ae00e0ba866dbc6a74b5ef5a553ea2ac911
Nabila Tamajnit <n.tamajnit.dev@gmail.com>
2026-01-07 10:55:11 +0100

    Add task.controller

---
+++ controllers/task.controller.js
@@ -0,0 +1,33 @@
+const taskController = {
+
+    getAll : (req, res) => {
+        res.sendStatus(501);
+    },
+
+    getById : (req, res) => {
+        res.sendteStatus(501);
+    },
+
+    getByUser : (req, res) => {
+        res.sendStatus(501);
+    },
+
+    insert : (req, res) => {
+        res.sendStatus(501);
+    },
+
+    update : (req, res) => {
+        res.sendStatus(501);
+    },
+
+    updateStatus : (req, res) => {
+        res.sendStatus(501);
+    },
+
+    delete : (req, res) => {
+        res.sendStatus(501);
+    }
+
+}
+
+module.exports = taskController;
