2e5c3bcf1f54fe918016ded367f815dee5cef95a
Nabila Tamajnit <n.tamajnit.dev@gmail.com>
2026-01-07 15:59:09 +0100

    Modification Taskcontroller with fakeTask service

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,28 +1,99 @@
+const { Request, Response } = require('express')
+
+const fakeTaskService = require("../services/fake/fakeTask.service");
+
 const taskController = {
 
+    /**
+     * Récupérer toutes les tâches
+     * @param { Request } req 
+     * @param { Response } res 
+     */
+
     getAll : (req, res) => {
-        res.sendStatus(501);
+        const tasks = fakeTaskService.find();
+
+        // Version 1
+        // res.status(200).json(tasks)
+
+        // Version 2
+        const dataToSend = {
+            count : tasks.length,
+            tasks : tasks
+        };
+
+        res.status(200).json(dataToSend);
     },
+    //? -------------------------------------------
+    /**
+     * Récupérer toutes les tâches
+     * @param { Request } req 
+     * @param { Response } res 
+     */
 
     getById : (req, res) => {
-        res.sendteStatus(501);
+        const id = +req.params.id;
+        const task = fakeTaskService.findById(id);
+
+        if(!task){
+            res.status(404).json({
+                                statusCode : 404,
+                                message : 'tâche non trouvée'
+                            })
+        }
+
+        res.status(200).json(task);
     },
+    //? -------------------------------------------
+    /**
+     * Récupérer toutes les tâches
+     * @param { Request } req 
+     * @param { Response } res 
+     */
 
     getByUser : (req, res) => {
         res.sendStatus(501);
     },
+    //? -------------------------------------------
+    /**
+     * Récupérer toutes les tâches
+     * @param { Request } req 
+     * @param { Response } res 
+     */
 
     insert : (req, res) => {
-        res.sendStatus(501);
+        const taskToAdd = req.body;
+        const addedTask = fakeTaskService.create(taskToAdd);
+
+        res.location(`/api/tasks/${addedTask.id}`);
+        res.status(201).json(addedTask);
     },
+    //? -------------------------------------------
+    /**
+     * Récupérer toutes les tâches
+     * @param { Request } req 
+     * @param { Response } res 
+     */
 
     update : (req, res) => {
         res.sendStatus(501);
     },
+    //? -------------------------------------------
+    /**
+     * Récupérer toutes les tâches
+     * @param { Request } req 
+     * @param { Response } res 
+     */
 
     updateStatus : (req, res) => {
         res.sendStatus(501);
     },
+    //? -------------------------------------------
+    /**
+     * Récupérer toutes les tâches
+     * @param { Request } req 
+     * @param { Response } res 
+     */
 
     delete : (req, res) => {
         res.sendStatus(501);
