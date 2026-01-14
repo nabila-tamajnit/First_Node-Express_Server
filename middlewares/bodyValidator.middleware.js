6a223709ef42a8ea3de2a7a42edb5469c08a69c8
Nabila Tamajnit <n.tamajnit.dev@gmail.com>
2026-01-14 16:04:12 +0100

    Debug bodyValidation

--- middlewares/bodyValidator.middleware.js
+++ middlewares/bodyValidator.middleware.js
@@ -8,25 +8,27 @@ const bodyValidatorMiddleware = () => {
      * @param {Request} req
      * @param {Response} res
      */
-    return(req, res, next) => {
+    return (req, res, next) => {
 
-        if(!req.body) {
+        if (!req.body) {
             next();
         }
 
         const fields = Object.keys(req.body);
 
-        for(let field of fields) {
+        for (let field of fields) {
 
             const valueInField = req.body[field];
 
-            if(typeof valueInField === 'string') {
+            if (typeof valueInField === 'string') {
 
-            if(offensivesWords.some(word => valueInField.toLocaleLowerCase().includes(word))) {
-                res.status(400).json( { statusCode : 400, message : `Vous ne pouvez pas mettre n'importe quoi dans ${field}`});
+                if (offensivesWords.some(word => valueInField.toLocaleLowerCase().includes(word))) {
+                    res.status(400).json({ statusCode: 400, message: `Vous ne pouvez pas mettre n'importe quoi dans ${field}` });
+                    return;
+                }
             }
         }
-        }
+        next();
     }
 }
 
