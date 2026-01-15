bcfa7f131ad456b31172edb9cb1ee52e5b1805d4
Nabila Tamajnit <n.tamajnit.dev@gmail.com>
2026-01-15 12:02:05 +0100

    Add mongoose + mongoDB connection

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const server = express();
 
-const { PORT } = process.env;
+const { PORT, DB_CONNECTION } = process.env;
 
 server.use(express.json());
 
+//? -----------Utilisation de l'app-middleware---------------
 const logMiddleware = require('./middlewares/log.middleware');
 server.use(logMiddleware());
+//? -----------------------------------------------------------
+
+//? ---------------------------------------Connexion DB-----------------------------------------
+const mongoose = require('mongoose')
+server.use( async (req, res, next) => {
+    try {
+        await mongoose.connect(DB_CONNECTION);
+        console.log("💾 Connection Successfully connect to the DB !");
+        next();
+        
+    } catch(err){
+        console.log(`❌ connection Failed \n[Reason]\n ${err}` );
+
+        res.status(500).json({ statusCode : 500, message : 'Impossible de se sonnecter à la base de données !' });
+    }
+})
+//? --------------------------------------------------------------------------------------------
 
 const router = require('./routes');
 server.use('/api', router);
