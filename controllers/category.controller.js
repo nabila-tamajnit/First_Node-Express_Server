fed2522bb877da628d43ff09609d119ef69d5283
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-01-21 18:48:39 +0100

    Add delete category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,102 +1,109 @@
-const { Request, Response } = require('express')
-
 const fakeCategoryService = require("../services/fake/fakeCategory.service");
 
-const categoryService = require('../services/mongo/category.service');
+const categoryService = require("../services/mongo/category.service");
 
 
 const categoryController = {
 
-    //? ======================GETALL===============================
-    /**
-     * Récupérer toutes les tâches
-     * @param { Request } req 
-     * @param { Response } res 
-     */
     getAll: async (req, res) => {
         try {
+            //on appelle notre service qui va chercher dans la DB
             const categories = await categoryService.find();
-
+            //Si ça marche, on renvoie les catégories
             res.status(200).json(categories);
+
         } catch (err) {
+            // si erreur de récupération dans le service, on envoie une 500
             console.log(err);
-            res.status(500).json({ statusCode: 500, message: 'erreur avec la DB' });
+            res.status(500).json({ statusCode: 500, message: 'Erreur avec la DB' });
 
         }
     },
-    //? ======================GETBYID===============================
-    /**
-     * Récupérer toutes les tâches
-     * @param { Request } req 
-     * @param { Response } res 
-     */
+
     getById: async (req, res) => {
+
         const id = req.params.id;
 
         try {
             const category = await categoryService.findById(id);
 
+            //Si category est undefined ou null
             if (!category) {
-                res.status(404).json({
-                    statusCode: 404,
-                    message: `La catégorie ${id} n\'exixte pas`
-                })
+                res.status(404).json({ statusCode: 404, message: `La catégorie ${id} n\'existe pas` })
             }
 
             res.status(200).json(category);
+
         }
         catch (err) {
-            console.log(err);
-            res.status(500).json({ statusCode: 500, message: 'Erreur de la DB' })
+            res.status(500).json({ statusCode: 500, message: 'Erreur de la DB' });
         }
+
     },
-    //? ======================INSERT===============================
-    /**
-     * Récupérer toutes les tâches
-     * @param { Request } req 
-     * @param { Response } res 
-     */
+
     insert: async (req, res) => {
         const categoryToAdd = req.body;
 
         try {
-            const exists = await categoryService.nameAlreadyExists(categoryToAdd.name);
+            // Si le nom existe déjà en base de données, erreur
+            const exists = await categoryService.nameAlreadyExists(categoryToAdd.name)
 
             if (exists) {
-                res.status(409).json({
-                    statusCode: 409,
-                    message: `La catégorie ${categoryToAdd.name} éxiste déjà`
-                })
+                res.status(409).json({ statusCode: 409, message: `La catégorie ${categoryToAdd.name} existe déjà !` });
             }
             else {
+                //Si elle n'existe pas, on peut la créer
                 const insertedCategory = await categoryService.create(categoryToAdd);
 
-                res.location(`/api/categories/${insertedCategory}`);
+                res.location(`/api/categories/${insertedCategory.id}`)
                 res.status(201).json(insertedCategory);
             }
-        }
-        catch(err) {
-            res.sendStatus(500);
 
+        } catch (err) {
+            res.sendStatus(500);
         }
+
+
     },
-    //? ======================UPDATE===============================
-    /**
-     * Récupérer toutes les tâches
-     * @param { Request } req 
-     * @param { Response } res 
-     */
+
     update: (req, res) => {
+        //TODO : Vérifier si l'id existe sinon 404
+
+        //TODO : Vérifier que le nouveau nom n'est pas déjà présent dans la db sinon 409
+
+        //TODO : Faire la modification
         res.sendStatus(501);
     },
-    //? ======================DELETE===============================
-    /**
-     * Récupérer toutes les tâches
-     * @param { Request } req 
-     * @param { Response } res 
-     */
-    delete: (req, res) => {
-        res.sendStatus(501);
+
+    delete: async (req, res) => {
+        try {
+
+            const categoryId = req.params.id;
+
+            //Si la catégorie est utilisée dans une tâche
+            if(await categoryService.isUsed(categoryId)){
+
+                res.status(409).json( {statusCode : 409, message : 'Suppression impossible, la catégorie est reliée à au moins une tâche'});
+
+            }else {
+                
+                // sinon, c'est bon, on tente la suppression
+                if(await categoryService.delete(categoryId)){
+                    //si suppression renvoie vrai, c'est bon c'est fait
+                    res.sendStatus(204);
+                }
+                else {
+                    //si suppression renvoie faux, l'id n'existait pas
+                    res.status(404).json({ statusCode : 404, message : 'La catégorie n\'existe pas'});
+                }
+
+            }
+
+        }catch(err){
+            console.log(err);
+            res.status(500).json({ statusCode : 500, message : 'Une erreur est survenue dans la DB' });
+        }
+
     },
 }
 
--- services/mongo/category.service.js
+++ services/mongo/category.service.js
@@ -1,62 +1,99 @@
 const Category = require("../../models/category.model");
+const Task = require('../../models/task.model');
 
 const categoryService = {
+    /* On va créer notre "vrai" service cette fois ci mais ce sont les mêmes fonctionnalités que notre fakeService, donc nous aurons les mêmes fonctions */
 
-    //? ======================FIND===============================
-    find : async() => {
+    find: async () => {
         try {
+            // On va interroger la db (ça peut prendre du temps ou planter donc c'est une promesse)
             const categories = await Category.find();
             return categories;
-        } 
-        catch(err) {
+        }
+        catch (err) {
             console.log(err);
             throw new Error(err);
         }
     },
-    //? ======================FINDBYID===============================
-    findById : async(id) => {
+
+    findById: async (id) => {
         try {
             const searchedCategory = await Category.findById(id);
             return searchedCategory;
-        } 
-        catch(err) {
+
+        } catch (err) {
             console.log(err);
             throw new Error(err);
-            
         }
     },
-    //? ======================CREATE===============================
-    create : async(category) => {
+
+    create: async (category) => {
         try {
+
+            // Dans category il y a des informations contenues dans le body
+            // On va créer l'objet à ajouter à partir du model qu'on a créé
             const categoryToAdd = Category(category);
+            // On va "sauvegarder" (c'est à dire insérer) notre category dans la db
             await categoryToAdd.save();
-
+            // Si tout s'est bien passé, on renvoie la category créée
             return categoryToAdd;
-        } 
-        catch(err) {
+
+        } catch (err) {
+
             console.log(err);
+            throw new Error(err);
+        }
+
+
+    },
+
+    delete : async (id) => {
+        try {
+            const deletedCategory = await Category.findByIdAndDelete(id);
+            if(!deletedCategory) return false;
+            else return true;
+        }
+        catch(err) {
             throw new Error(err);
         }
     },
 
-    //? ====================NAMEALREADYEXISTS============================
-    nameAlreadyExists : async(name) => {
+    nameAlreadyExists: async (name) => {
         try {
 
-            const searchedCategory = await Category.findOne( { name } );
-            
-            if(searchedCategory) {
+            //Trouver dans la db une categorie qui possède le nom reçu en paramètre
+            // const searchedCategory = await Category.findOne( { name : name } );
+            const searchedCategory = await Category.findOne({ name });
 
+            if (searchedCategory) {
+                // Si une catégorie a été trouvée c'est que le nom existait déjà donc on renvoie VRAI 
                 return true;
             }
             else {
+                // Si aucune catégorie n'a été trouvée c'est que le nom n'existait pas donc on renvoie FAUX 
                 return false;
             }
-        }
-        catch(err) {
+
+        } catch (err) {
+
             console.log(err);
+            throw new Error(err);
+
+        }
+    },
+
+    isUsed : async(id) => {
+        try {
+
+            // On essaie de récupérer un moins une tâche qui a cet id comme categoryId
+            const task = await Task.findOne( { categoryId : id } )
+            //Si pas de tâche, aucune n'est liée à cette catégorie donc on renvoie faux, elle n'es pas utilisée
+            if(!task) return false;
+            //Si y'a une tâche, au moins une est reliée donc on renvoie vrai
+            else return true; 
+
+        }catch(err){
             throw new Error(err)
-            
         }
     }
 }
