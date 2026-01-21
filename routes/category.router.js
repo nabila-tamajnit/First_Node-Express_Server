c3a0a5ce3051e8818bb6855e2e1b895913aad49a
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-01-21 12:27:14 +0100

    Add router authentication and roleAuthorization

--- routes/category.router.js
+++ routes/category.router.js
@@ -1,15 +1,29 @@
 const categoryController = require('../controllers/category.controller');
 const bodyValidatorMiddleware = require('../middlewares/bodyValidator.middleware');
 
+const authenticationMiddleware = require('../middlewares/auth/authentication.middleware');
+const roleAuthorizationMiddleware = require('../middlewares/auth/roleAuthorization.middleware');
+
 const categoryRouter = require('express').Router();
 
 categoryRouter.route('/')
     .get(categoryController.getAll)
-    .post(bodyValidatorMiddleware(), categoryController.insert)
+
+    .post(authenticationMiddleware(), 
+    roleAuthorizationMiddleware(['Admin']), 
+    bodyValidatorMiddleware(), 
+    categoryController.insert)
 
 categoryRouter.route('/:id')
     .get(categoryController.getById)
-    .put(bodyValidatorMiddleware(), categoryController.update)
-    .delete(categoryController.delete)
+
+    .put(authenticationMiddleware(), 
+    roleAuthorizationMiddleware(['Admin']), 
+    bodyValidatorMiddleware(), 
+    categoryController.update)
+
+    .delete(authenticationMiddleware(), 
+    roleAuthorizationMiddleware(['Admin']), 
+    categoryController.delete)
 
 module.exports = categoryRouter;
